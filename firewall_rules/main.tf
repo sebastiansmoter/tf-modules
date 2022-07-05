2b7ba7774f9f28cbdf36a8c0e41b4fdd38581206
sebastiansmoter <107245216+sebastiansmoter@users.noreply.github.com>
2022-07-05 13:34:14 +0200

    1

--- firewall_rules/main.tf
+++ firewall_rules/main.tf
@@ -2,9 +2,8 @@ terraform {
   required_version = "1.2.3"
 }
 
-resource "firewall_rules" "default"{
+resource "google_compute_firewall" "default"{
   name = var.firewall_name
-  source       = "terraform-google-modules/network/google//modules/firewall-rules"
   project_id = var.project_id
   network_name = var.network_name
 
