b08f95af67155a3bb3d7ecdd29dc26277d84d502
sebastiansmoter <107245216+sebastiansmoter@users.noreply.github.com>
2022-07-06 15:50:45 +0200

    1

--- cloud_nat/main.tf
+++ cloud_nat/main.tf
@@ -17,8 +17,8 @@ resource "google_compute_router" "my-router"{
 }
 resource "google_compute_router_nat" "nat" {
   name                               = "my-router-nat"
-  router                             = google_compute_router.router.name
-  region                             = google_compute_router.router.region
+  router                             = google_compute_router.my-router.name
+  region                             = google_compute_router.my-router.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
