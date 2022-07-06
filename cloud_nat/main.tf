75874b9b0fc763256cbe1d40e2606da9e83edd8f
sebastiansmoter <107245216+sebastiansmoter@users.noreply.github.com>
2022-07-06 15:49:55 +0200

    1

--- cloud_nat/main.tf
+++ cloud_nat/main.tf
@@ -21,7 +21,7 @@ resource "google_compute_router_nat" "nat" {
   region                             = google_compute_router.router.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-
+}
 
 
 
