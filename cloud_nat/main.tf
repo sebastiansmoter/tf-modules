14f7d8cbf21d52d0134e874fc7e28ca5608c8450
sebastiansmoter <107245216+sebastiansmoter@users.noreply.github.com>
2022-07-06 15:47:13 +0200

    1

--- cloud_nat/main.tf
+++ cloud_nat/main.tf
@@ -4,6 +4,8 @@ terraform {
 resource "google_compute_router" "my-router"{
   name = var.router_name
   network = var.network_name
+  region  = var.network_region
+
   bgp{
     asn = 64514
     advertise_mode = "CUSTOM"
@@ -13,7 +15,12 @@ resource "google_compute_router" "my-router"{
     }
   }
 }
-
+resource "google_compute_router_nat" "nat" {
+  name                               = "my-router-nat"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 
 
 
